24fa48ee6e2f4b6d05b4d4304d45c1f1bd5421d9
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2021-10-19 13:21:17 -0700

    Added functionality to push input values to currentLeads array

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-let inputBtn = document.getElementById("input-btn");
+//variables for program
+let myLeads = [];
 
-inputBtn.addEventListener("click", function () {
-  console.log("Button clicked from event listener!");
+//references to HTML element
+//set to const since these variables won't be reassigned later in program
+const saveBtn = document.getElementById("save-btn"); //refers to save
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+
+//functions/event listeners
+saveBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
 });
+
+//render function needed (implementation below)
+for (let i = 0; i < myLeads.length; i++) {
+  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
