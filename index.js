c2f8a447e21d163d16c34d7f6ff22583f889d0a3
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2021-10-20 11:31:48 -0700

    Added functionality for links to persist through refreshes using localStorage

--- index.js
+++ index.js
@@ -7,29 +7,31 @@ const saveBtn = document.getElementById("save-btn"); //refers to save
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 
+//handling any existing keys
+//var stores an array // grabbed from localStorage as string and parsed by JSON
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
 //functions/event listeners
 saveBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
+  myLeads.push(inputEl.value); //adds values to array which contains each URL
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)); //since localStorage only handles k/v pairs of strings, need to use JSON.stringify to convert array to string form
   renderLeads();
+  console.log(localStorage.getItem("myLeads"));
 });
 
 function renderLeads() {
   let listItems = "";
-  let a = document.createElement("a");
-
   for (let i = 0; i < myLeads.length; i++) {
-    // listItems +=
-    // "<li> <a target='_blank' href=' " +
-    // myLeads +
-    // "'>" +
-    // myLeads[i] +
-    // "</a></li>";
     listItems += `
     <li> 
         <a target='_blank' href='${myLeads[i]}'> ${myLeads[i]}</a>
     </li>`;
   }
-
   ulEl.innerHTML = listItems;
 }
