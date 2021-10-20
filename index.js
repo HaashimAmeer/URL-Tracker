7b46f6c8286b941275d8bb671ae08a03d74946dd
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2021-10-20 11:58:33 -0700

    Implemented "delete-all" btn to delete values from appropriate storage containers & clear DOM

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let myLeads = [];
 const saveBtn = document.getElementById("save-btn"); //refers to save
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 
 //handling any existing keys
 //var stores an array // grabbed from localStorage as string and parsed by JSON
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   renderLeads();
@@ -22,7 +22,13 @@ saveBtn.addEventListener("click", function () {
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads)); //since localStorage only handles k/v pairs of strings, need to use JSON.stringify to convert array to string form
   renderLeads();
-  console.log(localStorage.getItem("myLeads"));
+});
+
+//takes care of deleting values in localStorage, array within program, and clearing DOM
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads.splice(0, myLeads.length);
+  renderLeads();
 });
 
 function renderLeads() {
